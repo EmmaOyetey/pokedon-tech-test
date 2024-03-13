82a7ccfffc65468cfe742f77e6fb1a3d155c70b3
Emma Oyetey <emmaflookis@hotmail.com>
2024-03-13 11:37:18 +0000

    added const typestring to deal with the pokemon type array and ensure a '&' between types where multiple

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,16 @@ const pokemonContainer = document.querySelector<HTMLElement>(".card-container");
 if (!pokemonContainer)
 throw new Error ("couldn't get the details about the pokemon");
 
+
+
 pokemonContainer.innerHTML = "";
 pokemonArray.forEach((pokemon) : void => {
+    const typesString = pokemon.types.join(" & ");
     pokemonContainer.innerHTML += 
     `<div>
         <img class = "card__imag" src="${pokemon.sprite}" alt="image of pokemon">
         <h2 class = "card__heading">${pokemon.name}</h2>
-        <p class = "card__text">${pokemon.name} "is a " ${pokemon.types} "type pokemon""</p>;
+        <p class = "card__text">${pokemon.name} is a ${typesString} type pokemon </p>
 
 </div>`
 
