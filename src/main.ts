3a5f5b597fb9981d4f6d90599876b28c51de4653
Emma Oyetey <emmaflookis@hotmail.com>
2024-03-13 11:14:55 +0000

    code typed for pokemon containers not yet working on web

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
 import "./styles/style.scss";
+import {pokemon} from "./data/pokemon";
+
+
+const pokemonContainer = document.querySelector<HTMLElement>(".card-container");
+
+if (!pokemonContainer)
+throw new Error ("couldn't get the details about the pokemon");
+
+pokemonContainer.innerHTML = "";
+pokemon.forEach((pokemon) : void => {
+    pokemonContainer.innerHTML += 
+    `<div>
+        <img class = "card__imag" src="${pokemon.sprite}" alt="image of pokemon">
+        <h2 class = "card__heading">${pokemon.name}</h2>
+        <p class = "card__text">${pokemon.name} + "is a " + ${pokemon.types} "type pokemon""</p>;
+
+</div>`
+});
+
